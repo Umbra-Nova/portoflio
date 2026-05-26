f8b4370a1d1501d803de11bee8e799b2d00f649e
Jessica <j.socorro93@gmail.com>
2026-05-25 22:28:00 -0400

    chore(config): minor updates to the configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  // Look for test files in the "tests" directory, relative to this configuration file.
-  testDir: "tests",
+  // Look for test files in the 'tests' directory, relative to this configuration file.
+  testDir: 'tests',
 
   // Run all tests in parallel.
   fullyParallel: true,
@@ -17,29 +17,28 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   // Reporter to use
-  reporter: "html",
+  reporter: 'html',
 
   use: {
     // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: "http://localhost:3000",
+    baseURL: 'https://prime-lip.demo.prestashop.com',
 
     // Collect trace when retrying the failed test.
-    trace: "on-first-retry",
+    trace: 'on-first-retry',
   },
   // Configure projects for major browsers.
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
     },
     {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
     },
   ],
-
 });
